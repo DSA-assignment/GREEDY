c1a57d3b92ac1607b798a4dea2f0c14c2e319487
Navodyasankalani <56666880+Navodyasankalani@users.noreply.github.com>
2020-08-05 17:24:24 +0530

    Update Q3.c

--- Q3.c
+++ Q3.c
@@ -12,16 +12,14 @@ int main(){
 	}
 	
 	printf("\n");
-	for(i=0;i<n;i++){					// sort the weight sequence of products
+	for(i=0;i<n;i++){			// sort the weight sequence of products
 		for(j=i+1;j<n;j++){
 			if(arr[i]>arr[j]){
 				temp=arr[i];
 				arr[i]=arr[j];
 				arr[j]=temp;
-			}
-			
+			}	
 		}
-		
 	}
 	
 	for(i=0;i<n;i++){
